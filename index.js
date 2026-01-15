c8f284ba43bb91b0034feb5b9fd34ec6326d9d3e
Silas17 <silascs120@gmail.com>
2026-01-15 09:27:55 -0300

    projeto criado

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log
+console.log("Hello, World!");
+console.log("This is the index.js file.");
+console.log("Welcome to the project!");
+console.log("Let's get started with coding!");
+console.log("Enjoy your coding journey!");
